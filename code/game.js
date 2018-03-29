e13148331d75833bfa87490408cd30bda103c426
Yingying <37191990+milchbrotchen@users.noreply.github.com>
2018-03-29 17:45:49 -0400

    Lab 7

--- code/game.js
+++ code/game.js
@@ -1,3 +1,8 @@
+var actorChars = {
+  '@': Player,
+  'o': Coin,
+  'g': Ghost,
+};
 function Level(plan) {
   // Use the length of a single row to set the width of the level
   this.width = plan[0].length;
@@ -7,6 +12,7 @@ function Level(plan) {
 
   // Store the individual tiles in our own, separate array
   this.grid = [];
+  this.actors = [];
 
   // Loop through each row in the plan, creating an array in our grid
   for (var y = 0; y < this.height; y++) {
@@ -19,9 +25,10 @@ function Level(plan) {
 
       var ch = line[x], fieldType = null;
       // Use if and else to handle the three cases
-      if (ch==='@')
-        // Create a new player at that grid position.
-        this.player = new Player(new Vector(x, y));
+      var Actor = actorChars[ch];
+      if (Actor){
+        this.actors.push(new Actor(new Vector(x, y), ch));
+      }
       else if (ch == "x"){
         fieldType = "wall";
       }
@@ -39,6 +46,10 @@ function Level(plan) {
     // Push the entire row onto the array of rows.
     this.grid.push(gridLine);
   }
+
+  this.player = this.actors.filter(function(actor){
+    return actor.type == "player";
+  })[0];
 }
 
 function Vector(x, y) {
@@ -59,10 +70,25 @@ Vector.prototype.times = function(factor) {
 // A Player has a size, speed and position.
 function Player(pos) {
   this.pos = pos.plus(new Vector(0, -0.5));
-  this.size = new Vector(0.8, 1.5);
+  this.size = new Vector(1.05, 1);
   this.speed = new Vector(0, 0);
 }
 Player.prototype.type = "player";
+
+//add new actor as class
+function Coin(pos){
+  this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
+  this.size = new Vector(0.7, 0.7);
+  this.wobble = Math.random() * Math.PI * 2;
+}
+Coin.prototype.type = "coin";
+
+function Ghost(pos){
+  this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
+  this.size = new Vector(1, 1.0);
+  this.float = Math.random() * Math.PI * 2;
+}
+Ghost.prototype.type = "ghost";
 
 // Helper function to easily create an element of a type provided
 // and assign it a class.
@@ -107,25 +133,27 @@ DOMDisplay.prototype.drawBackground = function() {
   return table;
 };
 
-// Draw the player agent
-DOMDisplay.prototype.drawPlayer = function() {
+// Draw the actors
+DOMDisplay.prototype.drawActors = function() {
   // Create a new container div for actor dom elements
   var wrap = elt("div");
 
-  var actor = this.level.player;
-  var rect = wrap.appendChild(elt("div",
+  // Create a new element for each actor each frame
+  this.level.actors.forEach(function(actor) {
+    var rect = wrap.appendChild(elt("div",
                                     "actor " + actor.type));
-  rect.style.width = actor.size.x * scale + "px";
-  rect.style.height = actor.size.y * scale + "px";
-  rect.style.left = actor.pos.x * scale + "px";
-  rect.style.top = actor.pos.y * scale + "px";
+    rect.style.width = actor.size.x * scale + "px";
+    rect.style.height = actor.size.y * scale + "px";
+    rect.style.left = actor.pos.x * scale + "px";
+    rect.style.top = actor.pos.y * scale + "px";
+  });
   return wrap;
 };
 
 DOMDisplay.prototype.drawFrame = function() {
   if (this.actorLayer)
     this.wrap.removeChild(this.actorLayer);
-  this.actorLayer = this.wrap.appendChild(this.drawPlayer());
+  this.actorLayer = this.wrap.appendChild(this.drawActors());
   this.scrollPlayerIntoView();
 };
 
@@ -184,21 +212,51 @@ Level.prototype.obstacleAt = function(pos, size) {
   }
 };
 
+Level.prototype.actorAt = function(actor){
+  for (var i = 0; i < this.actors.length; i++) {
+    var other = this.actors[i];
+    if (other != actor &&
+        actor.pos.x + actor.size.x > other.pos.x &&
+        actor.pos.x < other.pos.x + other.size.x &&
+        actor.pos.y + actor.size.y > other.pos.y &&
+        actor.pos.y < other.pos.y + other.size.y)
+      return other;
+  }
+};
+
 // Update simulation each step based on keys & step size
 Level.prototype.animate = function(step, keys) {
 
   // Ensure each is maximum 100 milliseconds
   while (step > 0) {
     var thisStep = Math.min(step, maxStep);
-      this.player.act(thisStep, this, keys);
+    this.actors.forEach(function(actor) {
+      // Allow each actor to act on their surroundings
+      actor.act(thisStep, this, keys);
+    }, this);
    // Do this by looping across the step size, subtracing either the
    // step itself or 100 milliseconds
     step -= thisStep;
   }
 };
 
-var maxStep = 0.05;
+var wobbleSpeed = 8, wobbleDist = 0.07;
 
+Coin.prototype.act = function(step){
+  this.wobble += step * wobbleSpeed;
+  var wobblePos = Math.sin(this.wobble) * wobbleDist;
+  this.pos = this.basePos.plus(new Vector(0, wobblePos));
+};
+
+var floatSpd = 1.5, floatDist = 2;
+
+Ghost.prototype.act = function(step){
+  this.float += step * floatSpd;
+  var floatPos = Math.sin(this.float) * floatDist;
+  this.pos = this.basePos.plus(new Vector(floatPos, 0));
+}
+
+var maxStep = 0.05;
 var playerXSpeed = 5;
 
 Player.prototype.moveX = function(step, level, keys) {
@@ -213,11 +271,15 @@ Player.prototype.moveX = function(step, level, keys) {
   var obstacle = level.obstacleAt(newPos, this.size);
 
   // Move if there's not a wall there.
-  if(obstacle!="wall")
-    this.pos = newPos;
-  else if (obstacle == "floater"){
+  if (obstacle == "floater"){ //pass through floater
     this.pos = newPos;
   }
+  else if (obstacle == "lava"){
+    this.pos = new Vector (2, 5);
+  }
+  else if(obstacle!="wall")
+    this.pos = newPos;
+
 };
 
 var gravity = 28;
@@ -253,8 +315,30 @@ Player.prototype.moveY = function(step, level, keys) {
 Player.prototype.act = function(step, level, keys) {
   this.moveX(step, level, keys);
   this.moveY(step, level, keys);
+
+  var otherActor = level.actorAt(this);
+  if (otherActor)
+    level.playerTouched(otherActor.type, otherActor);
 };
 
+Level.prototype.restart = function(){
+  this.pos = new Vector (2, 5);
+}
+
+Level.prototype.playerTouched = function(type, actor) {
+  if (type == "coin"){
+    this.actors = this.actors.filter(function(other) {
+      return other != actor;
+    });
+  }
+
+  if (type == "ghost"){
+    //will cause gameover
+    this.actors = this.actors.filter(function(other) {
+      return other != actor;
+    });
+  }
+};
 
 // Arrow key codes for readibility
 var arrowCodes = {37: "left", 38: "up", 39: "right", 40: "down"};
